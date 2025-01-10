02471f43e58a041e994ae58167adfd02ffc5487c
Sigmowski25 <florexx410@gmail.com>
2025-01-10 22:14:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Słucha Minelany", "🎮 Playing Minelany"];
+const statusMessages = ["🎧 Słucha Minelany", "🎮 W Grze Minelany"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
